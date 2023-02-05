b32d83850fa1ee2490c6948aa1c3c7192a661fe5
ryanwright <ran@r-mor.com>
2023-02-05 11:05:45 +0200

    RV-42 added api scripts

--- dags/reconizer/scripts/api_scripts.py
+++ dags/reconizer/scripts/api_scripts.py
@@ -1,4 +1,18 @@
 """
     This file contains all modules that would collect data directly from api without connecting to new machines
 """
+import requests
 
+url = "https://haveibeenpwned.com/api/v3/breachedaccount/adobe"
+hibp_api_key = "you api goes here"
+payload = {}
+headers = {
+  'hibp-api-key': hibp_api_key,
+  'timeout': '2.5'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+data = response.json()
+for k, v in data.items():
+  print(k)
+  print(v)
