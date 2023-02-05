a3109efc934435e4cc470e9d3dcdf95493135001
ryanwright <ran@r-mor.com>
2023-02-05 13:43:42 +0200

    RV-42 moved kali scripts in the same scripts file

--- dags/reconizer/scripts/ec2_scripts.py
+++ dags/reconizer/scripts/ec2_scripts.py
@@ -17,3 +17,20 @@ def harvester_entrypoint(domain: str) -> dict:
     command = f'theHarvester -d {domain} -l 500 -b google'
     std_out, std_err = kali_machine_conn.run_command(command)
     return dict(error=std_err, response=std_out)
+
+
+def skip_fish_entrypoint(domain) -> dict:
+    """
+    Args:
+        domain: must be full like https://your-url.com
+    Returns:
+        dict: error and scan results
+    """
+    complete_fish_dict_path = "skipfish_dict/complete.wl"
+    scan_name = "skip_fish_results"
+    command = f'skipfish -o {scan_name} -S {complete_fish_dict_path} -u -k 05:00:00 {domain}'
+    kali_machine_conn.run_scan_on_remote_kali(command)
+    output_scan_filepath = f'{scan_name}/index.html'
+    result = kali_machine_conn.sftp_scan_results(output_scan_filepath)
+    kali_machine_conn.clean_scan_results(scan_name)
+    return dict(error=None, response=result)
