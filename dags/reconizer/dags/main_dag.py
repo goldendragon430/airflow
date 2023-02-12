077b930e725bfd0f5dc770ae3af0c93238b1a9b4
ryanwright <ran@r-mor.com>
2023-02-12 16:27:53 +0200

    RV-75 fixing bbot dependency from local constraints

--- dags/reconizer/dags/main_dag.py
+++ dags/reconizer/dags/main_dag.py
@@ -1,25 +1,30 @@
+import os
 from datetime import datetime
 
 from airflow.decorators import dag
 from airflow.models import Variable
 
 from reconizer.common.raw_data_operator import RawDataOperator
-from reconizer.scripts.api_scripts import apollo_entrypoint, have_i_been_pawned_entrypoint, signal_hire_entrypoint, \
-    view_dns_entrypoint, xforce_entrypoint
 from reconizer.scripts.bbot_scripts import shodan_dns_entrypoint, ssl_cert_entrypoint
-from reconizer.scripts.kali_scripts import ssl_scan_entrypoint, sublist3r_entrypoint_light, wafw00f_entrypoint, \
-    wapiti_entrypoint, wpscan_entrypoint
+from scripts.api_scripts import apollo_entrypoint, have_i_been_pawned_entrypoint, signal_hire_entrypoint, \
+    view_dns_entrypoint, xforce_entrypoint
+from scripts.kali_scripts import ssl_scan_entrypoint, sublist3r_entrypoint_light, wafw00f_entrypoint, wapiti_entrypoint, \
+    wpscan_entrypoint
 
 
 @dag(dag_id="main_dag", schedule_interval=None, start_date=datetime(2023, 1, 12))
 def main_dag():
+    path = os.path.join(os.getcwd(), "bbot_scans")
+    if not os.path.exists(path):
+        os.mkdir(path)
+
     # BBot scripts
     RawDataOperator(task_id="shodan_dns", fn=shodan_dns_entrypoint,
                     op_args=["www.ynet.co.il", Variable.get("secrets", deserialize_json=True).get("shodan_dns")])
 
     RawDataOperator(task_id="sslcert", fn=ssl_cert_entrypoint, op_args=["www.ynet.co.il"])
 
-    # Api scripts
+    # # Api scripts
 
     RawDataOperator(task_id="haveibeenpawned", fn=have_i_been_pawned_entrypoint,
                     op_args=["www.ynet.co.il", Variable.get("secrets", deserialize_json=True).get("haveibeenpawned")])
@@ -38,7 +43,7 @@ def main_dag():
                     op_args=["www.ynet.co.il", Variable.get("secrets", deserialize_json=True).get("xforce_key"),
                              Variable.get("secrets", deserialize_json=True).get("xforce_pass")])
 
-    # Kali scripts
+    # # Kali scripts
 
     RawDataOperator(task_id="wpscan", fn=wpscan_entrypoint,
                     op_args=["https://snoopdogg.com/", Variable.get("secrets", deserialize_json=True).get("wp_scan")])
