08e0488fc07f961b02240a4f28dbbc511ef96793
ryanwright <ran@r-mor.com>
2023-02-23 13:30:23 +0200

    RV-112v3 raw data

--- dags/reconizer/scripts/bbot_scripts.py
+++ dags/reconizer/scripts/bbot_scripts.py
@@ -88,29 +88,13 @@ def bbot_events_iteration(domain: str, secrets: dict, start: int = 0, end: int =
     if scan.status == "FINISHED":
         events = get_scan_result(filepath=f'{scan_name}/output.json', mode="json")
 
-        # don't write raw data to S3 for now
-        # raw_data = []
-        # for record in events[1:]:
-        #     raw_data.append({k: record[k] for k in ('type', 'data', 'resolved_hosts', 'tags', 'module') if k in record})
+        raw_data = []
+        for record in events[1:]:
+            raw_data.append({k: record[k] for k in ('type', 'data', 'resolved_hosts', 'tags', 'module') if k in record})
 
         # filter and group by event type
         grouped_by = filtered_events(events)
         clean_scan_folder(scan_name)
-        return grouped_by
+        return raw_data
     else:
         return {}
-
-#
-# sec = {"apollo": "z9tV-4JdDnurSiFrTBItMA", "app_brain": "p24594.0rke2dkkh9ec0rn2mo76d",
-#        "google": "AIzaSyA4AFdjloj5QVh1FvtE_Th-ayONGZKs22w", "haveibeenpawned": "42806857f18943debadc4156ea892c44",
-#        "mx": "66675eb1-cd13-493f-824e-a77f039cb53e", "rocketreach": "7fc8e4kc711fe050dccc9c83883a53edd280e64",
-#        "shodan_dns": "slf8J3ML9slEOmuBahHYhudCgD73rVgb", "signal": "202..nc7AFDvB1kibYsFI0jUuyHwDKcy",
-#        "spyse": "80f45c86-7a17-4c1e-8968-a5d885b9a3d5", "view_dns": "483a6a44a649e4f95821147da90a130ea44ad1c1",
-#        "wp_scan": "0aKpORSMkvP60g34PWXAWk4Ev7iWma4bLkyDvubu8q8", "censys_key": "bc3e9cbd-a04d-4ce8-afda-7c15e64d56f0",
-#        "censys_pass": "MNh6gcijt5jiynXi32LJQ1MDrMzU1Vwg", "xforce_key": "eefc9616-7f87-4290-9965-7ce53e22bbd3",
-#        "xforce_pass": "4be25c9c-ffa3-40e3-8a9c-017ba332466a"}
-#
-# d = bbot_events_iteration("zap.co.il", sec, 12, 18)
-# for k, v in d.items():
-#     print(k)
-#     print(v)
